1f52af8a77cab354b836806081268676f0636f10
raha <shahsavanraha@gmail.com>
2024-10-05 08:45:27 +0330

    Update DFS algorithm

--- DFS.py
+++ DFS.py
@@ -5,13 +5,18 @@ def DFS_traversal(G, nodes, start):
     traversal_list = []
     start_index = nodes.index(start)
     stack = [start_index]
-    
+    count=0
     while stack:
+        count+=1
+        if count >15:
+            break
         node = stack.pop()
+        
         
         if node not in visited:
             # Visit the node and add to traversal
             traversal_list.append(nodes[node])
+            visited.add(node)
             
         
             # Find neighbors and push them to the stack
@@ -21,8 +26,37 @@ def DFS_traversal(G, nodes, start):
     
     return traversal_list
 
-            
-       
+
+# --- DFS_traversal , none-recursive ,input : Graph:{"node":["adjacent node",...],...}  , start node          
+
+def DFS_traversal_G(G,start):
+    visited=set()
+    stack=[start]
+    traversal=[]
+    count=0
+    while stack:
+        count+=1
+        if count >15:
+            break
+        node=stack.pop() 
+        traversal.append(node)
+        visited.add(nodes)
+        for nodes in G["node"] :
+            if nodes not in visited :
+                stack.append(nodes)
+    return traversal
+                
+              
+
+
+
+
+
+
+
+
+
+
 A=[[0,1,1,1,0,0],
    [1,0,0,0,1,0],
    [1,0,0,0,0,1],
@@ -32,3 +66,13 @@ A=[[0,1,1,1,0,0],
 nodes=["a","b","c","d","e","f"]
 
 print(DFS_traversal(A,nodes,"a"))
+
+# G={"a":["b","c","d"],
+#    "b":["a","e"],
+#    "c":["a","f"],
+#    "d":["a","f"],
+#    "e":["b"],
+#    "f":["c","d"]
+#    }
+
+# print(DFS_traversal_G(G,"a"))
